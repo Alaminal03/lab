a4a5926b2b81b78ee5d8ba9fb8edf17a77c13789
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-08-24 12:15:52 +0600

    Update ds_3.h

--- ds_3.h
+++ ds_3.h
@@ -1,137 +1,95 @@
-void one_d_int_array_display(int a[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-}
-
-void OneINDarray(int a[], int n)
-{
-    printf("Enter %d elements:\n", n);
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-}
-int ONEDINTArrayInput_LP(int aa[],int nn,int vv)
-{
-
-    aa[nn]=vv;
-    return nn+1;
-}
-int ONEDINTArrayInput_SP(int aa[50],int nn,int vv,int pp)
-{
-    int i;
-    for (i = nn; i >= pp; i--)
-    {
-        aa[i + 1] = aa[i];
-        
-    }
-    aa[pp] = vv;
-    return nn + 1;
-}
-int oneDIntDelSP(int aa[50],int nn,int pp)
-{
-    int i;
-    for (i = pp; i < nn;i++)
-    {
-        aa[i] = aa[i + 1];
-    }
-    return nn - 1;
-}
-void OneIntArrayLeanerSearch(int aa[50],int nn,int vv)
-{
-    int i, c = 0;
-    for (i = 1; i < nn;i++)
-    {
-        if(vv==aa[i])
-        {
-            printf("\nFound at position = %d\n", i);
-            c = c + 1;
-        }
-    }
-    if(c==0)
-    {
-        printf("\nNot Found");
-    }
-    else
-    {
-        printf("\nTotal Found = %d\n", c);
-    }
-}
- void OneDIntArrayBubble_Sort(int aa[50], int nn)
- {
-     int i, j, temp;
-     for (int i = 1; i <= nn;i++)
-     {
-         for (j = i; j <= nn;j++)
-         {
-            if(aa[j] > aa[j+1])
-            {
-                temp = aa[j];
-                aa[j] = aa[j + 1];
-                aa[j + 1] = temp;
-            }
-
-         }
-     }
- }
-void OneDIntArrayBinary_Search(int aa[50],int nn,int vv)
- {
-     int beg, end, mid;
-     OneDIntArrayBubble_Sort(aa, nn);
-     beg = 0;
-     end = nn-1;
-     
-    while(beg<=end)
-    {
-        mid = (beg + end) / 2;
-        if(vv==aa[mid])
-        {
-            printf("\nFound");
-            return;
-        }
-        else if(vv>aa[mid])
-        {
-            beg = mid + 1;
-        }
-        else if(vv<aa[mid])
-        {
-            end = mid - 1;
-        }
-    }
-
-     struct node
+struct node
 {
     int value;
-    struct node *add;
+    struct node *address;
 };
 
-void LinkedList_InsertionLast( struct node *l,int vv)
-{
-   struct node *temp;
-   while(l->add!=NULL)
-    l=l->add;
-   temp=(struct node *)malloc(sizeof(struct node));
-   temp->value=vv;
-   temp->add=l->add;
-   l->add=temp;
-}
+void LinkedList_InsertionLast(struct node *list, int v)
+ {
+     struct node* tmp;
+     while(list->address != NULL)
+     {
+         list = list->address;
+     }
+     tmp = (struct node *)malloc(sizeof(struct node));
+     tmp->value = v;
+     tmp->address = list->address;
+     list->address = tmp;
+ }
 
- void LinkedList_Display(struct node *l)
-
+void LinkedList_Display(struct node *list)
 {
-    while(l->add!=NULL)
+    while(list->address != NULL)
     {
-        printf("\t->%d",l->add->value);
-        l=l->add;
+        printf("\t->%d", list->address->value);
+        list = list->address;
     }
 }
-    printf("\nNot Found");
-            
-                
-            
+
+int LinkedList_CountNode(struct node* list)
+{
+    int count = 0;
+    while(list->address != NULL)
+    {
+        count++;
+        list = list->address;
+    }
+    return count;
+}
+
+void LinkedList_LinearSearch(struct node* list, int vv)
+{
+    int flag = 0;
+    while(list->address != NULL)
+    {
+        if(list->address->value == vv)
+        {
+            printf("\nFound\n");
+            flag = 1;
+            break;
+        }
+        list = list->address;
+    }
+
+    if(flag == 0)
+    {
+        printf("\nNot Found\n");
+    }
+}
+
+void LinkedList_InsertSpecific(struct node* list, int pp, int vv)
+{
+    int c = 0;
+    struct node* tmp;
+    while(list->address != NULL)
+    {
+        if(c == pp-1)
+            break;
+        list = list->address;
+        c = c+1;
+    }
+    tmp = (struct node*)malloc(sizeof(struct node));
+    tmp->value = vv;
+    tmp->address = list->address;
+    list->address = tmp;
+}
+
+void LinkedList_DeleteSpecific(struct node* list, int pp)
+{
+    int c = 0;
+    struct node* tmp;
+    while(list->address != NULL)
+    {
+        if(c == pp-1)
+            break;
+        list = list->address;
+        c = c+1;
+    }
+    tmp = list->address;
+    list->address = tmp->address;
+    printf("\nDelete %d from position %d : ", tmp->value, pp);
+
+
+    // return memory to computer
+    free(tmp);
 }
